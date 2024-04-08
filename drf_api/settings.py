ad3212e0c2ff50027234338e20232f7d8ab3295e
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-08 15:14:20 +0000

    ready for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-alanpaterson-djangorest-0yj0z1q8uhy.ws-eu110.gitpod.io', 'https://django-rest-ap-9a62d525c1f0.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-alanpaterson-djangorest-0yj0z1q8uhy.ws-eu110.gitpod.io', 'https://django-rest-ap-9a62d525c1f0.herokuapp.com']
 
 
 # Application definition
