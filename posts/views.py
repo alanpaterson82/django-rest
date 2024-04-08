8229d409e570cbb76ad602e849f1817cc28d7f95
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-08 14:37:02 +0000

    ready for deployment

--- posts/views.py
+++ posts/views.py
@@ -26,7 +26,6 @@ class PostList(generics.ListCreateAPIView):
         'owner__followed__owner__profile',
         'likes__owner__profile',
         'owner__profile',
-        'owner__followed__owner__profile',
     ]
     search_fields = [
         'owner__username',
