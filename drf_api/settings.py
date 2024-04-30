157544e2dcb06f8079e05443d1ec87663c88172f
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-30 19:50:15 +0000

    combining projects final step

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -1,5 +1,3 @@
-import re
-
 """
 Django settings for drf_api project.
 
@@ -60,11 +58,12 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '8000-alanpaterson-djangorest-0yj0z1q8uhy.ws-eu110.gitpod.io',
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
 ]
-
 
 # Application definition
 
@@ -106,15 +105,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CLIENT_ORIGIN')
+]
 
-CORS_ALLOWED_ORIGINS = ['https://react-project5-1b55a97836d1.herokuapp.com', ]
-
-CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
 
