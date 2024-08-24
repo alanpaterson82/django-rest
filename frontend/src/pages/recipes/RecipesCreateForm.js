b86c740339327cfca0609068d4f3db3df0b883d4
Alan Paterson <alan_paterson82@hotmail.com>
2024-08-24 16:30:44 +0000

    add recipe form

--- frontend/src/pages/recipes/RecipesCreateForm.js
+++ frontend/src/pages/recipes/RecipesCreateForm.js
@@ -1,20 +1,26 @@
-import Card from 'react-bootstrap/Card';
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import React from 'react';
 
-function RecipesCreateForm() {
+function ContactCreateForm() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-  );
+    <Form>
+        <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Recipe Name</Form.Label>
+        <Form.Control type="text" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Ingredients</Form.Label>
+        <Form.Control as="textarea" rows={10} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Method</Form.Label>
+        <Form.Control as="textarea" rows={10} />
+      </Form.Group>
+      <Button variant="secondary">Post</Button>{' '}
+    </Form>
+    );
 }
 
-export default RecipesCreateForm;
+
+export default ContactCreateForm;
